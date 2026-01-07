96f6d96ffc61b1be1fccb0c524837e4b9f60f9a1
NOVVAH <h_az_2000@hotmail.co.uk>
2026-01-07 21:48:17 +0000

    Possibly working needs testing as unsure how Java handles adding variables to class via a mixin

--- src/main/java/net/torchednova/nnindustextras/mixin/tomscraftingmixin.java
+++ src/main/java/net/torchednova/nnindustextras/mixin/tomscraftingmixin.java
@@ -1,61 +1,88 @@
 package net.torchednova.nnindustextras.mixin;
 
-import com.tom.storagemod.StorageMod;
-import com.tom.storagemod.menu.CraftingTerminalMenu;
-import com.tom.storagemod.polymorph.PolymorphHelper;
-import net.minecraft.network.protocol.game.ClientboundContainerSetSlotPacket;
+import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.ContainerListener;
-import net.minecraft.world.inventory.Slot;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.CraftingContainer;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.crafting.CraftingInput;
 import net.minecraft.world.item.crafting.CraftingRecipe;
 import net.minecraft.world.item.crafting.RecipeHolder;
-import net.minecraft.world.item.crafting.RecipeType;
-import net.torchednova.nnindustextras.ItemsStageController;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.lang.ref.WeakReference;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Optional;
 
 import static net.torchednova.nnindustextras.NNIndustExtras.LOGGER;
 
 @Mixin(targets = "com.tom.storagemod.block.entity.CraftingTerminalBlockEntity")
-public class tomscraftingmixin {
+public abstract class tomscraftingmixin {
 
+    private HashMap<Player, Inventory> inventoryViewers = new HashMap<>();
+
+    @Unique
+    private Player nNIndustExtras$temp;
+
+    @Shadow
+    private boolean refillingGrid;
+
+    @Final
+    @Shadow
+    private CraftingContainer craftMatrix;
+
+    @Shadow
+    private Optional<RecipeHolder<CraftingRecipe>> currentRecipe;
 
     @Shadow
     private WeakReference<Player> polymorphPlayer;
 
     @Inject(
-            method = "getRecipe",
+            method = "createMenu",
+            at = @At("HEAD")
+    )
+    public void nnindustextras$createMenu(int id, Inventory plInv, Player arg2, CallbackInfoReturnable<AbstractContainerMenu> cir) {
+        nNIndustExtras$temp = arg2;
+    }
+
+    @Inject(
+            method = "onCraftingMatrixChanged",
             at = @At("HEAD"),
             cancellable = true
     )
-    public void nnindustextras$onTake(CraftingInput input, CallbackInfoReturnable<Optional<RecipeHolder<CraftingRecipe>>> cir) {
-        LOGGER.info("here");
+    public void nnindustextras$onCraftingMatrixChanged(CallbackInfo ci) {
+        LOGGER.info("onCraftingMatrixChanged here1");
+        if (refillingGrid) {
+            return;
+        }
+        if (nNIndustExtras$temp == null)
+        {
+            ci.cancel();
+        }
+        LOGGER.info(nNIndustExtras$temp.toString());
 
-        if (StorageMod.polymorph && this.polymorphPlayer != null) {
-            Player player = this.polymorphPlayer.get();
-            LOGGER.info("here1");
-            CraftingRecipe cr =  PolymorphHelper.getRecipe(player, RecipeType.CRAFTING, input, player.level()).get().value();
-            LOGGER.info("here3");
-            if (!cr.isIncomplete())
-            {
-                LOGGER.info("here4");
-                ResourceLocation id = cr.getResultItem(player.level().registryAccess()).getItem().builtInRegistryHolder().key().location();
-                LOGGER.info(id.toString());
-            }
+        LOGGER.info("onCraftingMatrixChanged here2");
+        CraftingInput input = craftMatrix.asCraftInput();
+
+        LOGGER.info("onCraftingMatrixChanged here3");
+        if (currentRecipe.isEmpty()) {
+            return;
         }
 
+
+        LOGGER.info("onCraftingMatrixChanged here4");
+        Item item = currentRecipe.get().value().assemble(input, Minecraft.getInstance().level.registryAccess()).getItem();
+        ResourceLocation id = item.builtInRegistryHolder().key().location();
+
+        LOGGER.info("ID: {}", id);
     }
 }
